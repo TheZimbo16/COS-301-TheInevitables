ff282e8585a6029bf06363fac632a73bdd7dd380
Dawie Pritchard <dawiepritchard@gmail.com>
2017-07-18 09:00:40 +0200

    this is the app we are working on
    Former-commit-id: d6258b46ef60535bd55510814f980570746a73d8

---
+++ ExampleJEEApplication/rest/src/main/java/com/epiuse/tenderplatform/web/api/dto/problem/DTO.java
@@ -0,0 +1,15 @@
+package com.epiuse.tenderplatform.web.api.dto.problem;
+
+public abstract class DTO {
+
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
