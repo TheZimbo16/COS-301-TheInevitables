aaaaac1896a6f80a7f7cf133f0bf239d63c2b0d2
Drew Langley <drewla.the16@gmail.com>
2017-07-23 11:09:40 +0200

    updates to app frontend
    Former-commit-id: 7b4d698bcee53b9961fa7ea1c9c7aed860db51fa

--- NavUP/Frontend/www/js/app.js
+++ NavUP/Frontend/www/js/app.js
@@ -1,63 +1,121 @@
-// angular.module('myApp', ['ionic', 'myApp.controllers', 'myApp.directives'])
-//
-// .run(function($ionicPlatform) {
-//   $ionicPlatform.ready(function() {
-//     if(window.StatusBar) {
-//       StatusBar.styleDefault();
-//     }
-//   });
-// })
+angular.module('myApp', ['ionic', 'myApp.controllers', 'myApp.directives'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.search', {
+    url: '/search_map',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
+      }
+    }
+  })
+
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});
+
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var myApp = angular.module('myApp', []);
-myApp.controller('appCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
-
-
-var refresh = function() {
-  $http.get('/contactlist').success(function(response) {
-    console.log("I got the data I requested");
-    $scope.contactlist = response;
-    $scope.contact = "";
-  });
-};
-
-refresh();
-
-$scope.addContact = function() {
-  console.log($scope.contact);
-  $http.post('/contactlist', $scope.contact).success(function(response) {
-    console.log(response);
-    refresh();
-  });
-};
-
-$scope.remove = function(id) {
-  console.log(id);
-  $http.delete('/contactlist/' + id).success(function(response) {
-    refresh();
-  });
-};
-
-$scope.edit = function(id) {
-  console.log(id);
-  $http.get('/contactlist/' + id).success(function(response) {
-    $scope.contact = response;
-  });
-};
-
-$scope.update = function() {
-  console.log($scope.contact._id);
-  $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
-    refresh();
-  })
-};
-
-$scope.deselect = function() {
-  $scope.contact = "";
-}
-
-}]);﻿
+// var myApp = angular.module('myApp', []);
+// myApp.controller('appCtrl', ['$scope', '$http', function($scope, $http) {
+//     console.log("Hello World from controller");
+//
+//
+// var refresh = function() {
+//   $http.get('/contactlist').success(function(response) {
+//     console.log("I got the data I requested");
+//     $scope.contactlist = response;
+//     $scope.contact = "";
+//   });
+// };
+//
+// refresh();
+//
+// $scope.addContact = function() {
+//   console.log($scope.contact);
+//   $http.post('/contactlist', $scope.contact).success(function(response) {
+//     console.log(response);
+//     refresh();
+//   });
+// };
+//
+// $scope.remove = function(id) {
+//   console.log(id);
+//   $http.delete('/contactlist/' + id).success(function(response) {
+//     refresh();
+//   });
+// };
+//
+// $scope.edit = function(id) {
+//   console.log(id);
+//   $http.get('/contactlist/' + id).success(function(response) {
+//     $scope.contact = response;
+//   });
+// };
+//
+// $scope.update = function() {
+//   console.log($scope.contact._id);
+//   $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+//     refresh();
+//   })
+// };
+//
+// $scope.deselect = function() {
+//   $scope.contact = "";
+// }
+//
+// }]);﻿
