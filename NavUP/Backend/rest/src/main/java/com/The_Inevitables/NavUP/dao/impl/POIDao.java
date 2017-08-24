453d7d562ae498c09483122c369a22899970eae9
Dawie <you@example.com>
2017-08-24 21:47:22 +0200

    backend with poi working

---
+++ NavUP/Backend/rest/src/main/java/com/The_Inevitables/NavUP/dao/impl/POIDao.java
@@ -0,0 +1,16 @@
+package com.The_Inevitables.NavUP.dao.impl;
+
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+import com.The_Inevitables.NavUP.model.ProblemStatement;
+import com.The_Inevitables.NavUP.model.POI;
+
+@Stateless
+@LocalBean
+public class POIDao extends AbstractDaoImpl<POI>  {
+
+
+}
